bea63afc690f4d0eb5649592b994762b3a641968
IliykhaDobrinskiy <bogdobrinskiy@gmail.com>
2021-10-19 19:12:56 +0300

    Created MyFrame class

---
+++ src/MainPackege/MyFrame.java
@@ -0,0 +1,14 @@
+package MainPackege;
+
+import javax.swing.*;
+
+public class MyFrame extends JFrame{
+    MyFrame(){
+        setTitle("Calculator");
+        setSize(420,420);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+
+    }
+}
