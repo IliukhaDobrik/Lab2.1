191d86bfe59140e5e4b1e5b0af74b12b96e4daf3
IliykhaDobrinskiy <bogdobrinskiy@gmail.com>
2021-11-12 19:42:03 +0300

    ADDED memory

--- src/MainPackege/MyFrame.java
+++ src/MainPackege/MyFrame.java
@@ -4,16 +4,16 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
 
 public class MyFrame extends JFrame{
-    private int WIDTH = 700;
-    private int HEIGHT = 500;
+    private final int WIDTH = 700;
+    private final int HEIGHT = 500;
 
     private JTextField textFieldX = new JTextField();
     private JTextField textFieldY = new JTextField();
     private JTextField textFieldZ = new JTextField();
     private JTextField textFieldResult = new JTextField();
+    private JTextField textFieldMem = new JTextField();
 
     private ButtonGroup radioButtons = new ButtonGroup();
 
@@ -21,6 +21,11 @@ public class MyFrame extends JFrame{
     private Box boxFormula2 = Box.createVerticalBox();
 
     private int formulaID = 1;
+
+    private int memID = 1;
+    private Double mem1 = 1d;
+    private Double mem2 = 2d;
+    private Double mem3 = 3d;
 
     MyFrame(){
         setTitle("Калькулятор");
@@ -74,6 +79,71 @@ public class MyFrame extends JFrame{
         boxResult.add(Box.createHorizontalGlue());
         boxResult.setBorder(BorderFactory.createLineBorder(Color.BLUE,2));
 
+        Box boxMemory = Box.createVerticalBox();
+        Box boxRadio = Box.createHorizontalBox();
+        boxRadio.add(Box.createHorizontalGlue());
+        boxRadio.add(Box.createHorizontalStrut(10));
+        addRadioButton("Переменная 1", boxRadio, 1);
+        boxRadio.add(Box.createHorizontalStrut(80));
+        addRadioButton("Переменная 2", boxRadio, 2);
+        boxRadio.add(Box.createHorizontalStrut(80));
+        addRadioButton("Переменная 3", boxRadio, 3);
+        boxRadio.add(Box.createHorizontalStrut(10));
+        boxRadio.add(Box.createHorizontalGlue());
+        Box boxField = Box.createHorizontalBox();
+        boxField.add(Box.createHorizontalGlue());
+        textFieldMem = new JTextField("0",10);
+        textFieldMem.setMaximumSize(textFieldY.getPreferredSize());
+        boxField.add(textFieldMem);
+        boxField.add(Box.createHorizontalGlue());
+        boxMemory.add(boxRadio);
+        boxMemory.add(boxField);
+        boxMemory.setBorder(BorderFactory.createLineBorder(Color.cyan,2));
+
+        JButton memCalc = new JButton("M+");
+        memCalc.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    double x = Double.parseDouble(textFieldX.getText());
+                    double y = Double.parseDouble(textFieldY.getText());
+                    double z = Double.parseDouble(textFieldZ.getText());
+                    if (formulaID==1){
+                        if (memID == 1){
+                            mem1+=formula1(x,y,z);
+                            textFieldMem.setText(mem1.toString());
+                        }
+                        if (memID == 2){
+                            mem2+=formula1(x,y,z);
+                            textFieldMem.setText(mem2.toString());
+                        }
+                        else {
+                            mem3+=formula1(x,y,z);
+                            textFieldMem.setText(mem3.toString());
+                        }
+                    }
+                    else {
+                        if (memID == 1){
+                            mem1+=formula2(x,y,z);
+                            textFieldMem.setText(mem1.toString());
+                        }
+                        if (memID == 2){
+                            mem2+=formula2(x,y,z);
+                            textFieldMem.setText(mem2.toString());
+                        }
+                        else {
+                            mem3+=formula2(x,y,z);
+                            textFieldMem.setText(mem3.toString());
+                        }
+                    }
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(MyFrame.this,
+                            "Ошибка в формате записи числа с плавающей точкой", "Ошибочный формат числа",
+                            JOptionPane.WARNING_MESSAGE);
+                }
+            }
+        });
+
         JButton buttonCalc = new JButton("Вычислить");
         buttonCalc.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ev) {
@@ -107,6 +177,8 @@ public class MyFrame extends JFrame{
 
         Box boxButtons = Box.createHorizontalBox();
         boxButtons.add(Box.createHorizontalGlue());
+        boxButtons.add(memCalc);
+        boxButtons.add(Box.createHorizontalStrut(30));
         boxButtons.add(buttonCalc);
         boxButtons.add(Box.createHorizontalStrut(30));
         boxButtons.add(buttonReset);
@@ -119,6 +191,7 @@ public class MyFrame extends JFrame{
         contentBox.add(boxFormula1);
         contentBox.add(boxFormula2);
         contentBox.add(boxVariables);
+        contentBox.add(boxMemory);
         contentBox.add(boxResult);
         contentBox.add(boxButtons);
         contentBox.add(Box.createVerticalGlue());
@@ -162,9 +235,32 @@ public class MyFrame extends JFrame{
         box.add(button);
     }
 
+    private void addRadioButton(String buttonName,Box box,final int memID){
+        JRadioButton button = new JRadioButton(buttonName);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (memID == 1){
+                    MyFrame.this.memID = memID;
+                    textFieldMem.setText(mem1.toString());
+                }
+                if (memID == 2){
+                    MyFrame.this.memID = memID;
+                    textFieldMem.setText(mem2.toString());
+                }
+                if (memID == 3){
+                    MyFrame.this.memID = memID;
+                    textFieldMem.setText(mem3.toString());
+                }
+            }
+        });
+        radioButtons.add(button);
+        box.add(button);
+    }
+
     public double formula1(double x, double y, double z){
         double q = Math.sin(Math.log(y) + Math.sin(Math.PI * y*y));
-        double w = Math.pow(x*x+ Math.sin(z) + Math.pow(Math.E, Math.cos(z)),1/4);
+        double w = Math.pow(x*x+ Math.sin(z) + Math.pow(Math.E, Math.cos(z)),0.25);
         return q*w;
     }
 
